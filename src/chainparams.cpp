917d16ab5e4113975124b7d3b8d306e86858e460
ckti <ckti@i2pmail.org>
2022-04-02 13:40:11 +0100

    Update Wagerr reward structure
    - Update post-fork block reward schedule
    - Add initial carbon offset address
    - Store reward flags in CbTx to help verify and identify rewards across outputs
    - Fix PoW reward distribution after DIP0003 activation
    - Add Operator rewards to CBlockReward

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -390,6 +390,7 @@ public:
         consensus.nStakeMinAge = 60 * 60; // 1 hour
         consensus.nBlockStakeModifierV1A = 1000;
         consensus.nBlockStakeModifierV2 = consensus.V16DeploymentHeight;
+        consensus.strCarbonOffsetAddress = "sYG1qGUtbTdNRYtFsKvnY3GvuauF3eVwhT";
         // ATP parameters
         consensus.ATPStartHeight = std::numeric_limits<int64_t>::max();
         consensus.WgrAddrPrefix = "wgr";
@@ -583,6 +584,7 @@ public:
         consensus.nStakeMinAge = 60 * 60; // 1 hour
         consensus.nBlockStakeModifierV1A = 51197;
         consensus.nBlockStakeModifierV2 = std::numeric_limits<int>::max();
+        consensus.strCarbonOffsetAddress = "TkDutp66Ygp5PpPnrETvfyrtnxq5UevLpo";
         // ATP parameters
         consensus.ATPStartHeight = consensus.V16DeploymentHeight;
         consensus.WgrAddrPrefix = "wgrtest";
@@ -760,6 +762,7 @@ public:
         consensus.nStakeMinAge = 60 * 60; // 1 hour
         consensus.nBlockStakeModifierV1A = 1000;
         consensus.nBlockStakeModifierV2 = consensus.V16DeploymentHeight;
+        consensus.strCarbonOffsetAddress = "TkDutp66Ygp5PpPnrETvfyrtnxq5UevLpo";
         // ATP parameters
         consensus.ATPStartHeight = consensus.V16DeploymentHeight;
         consensus.WgrAddrPrefix = "wgrtest";
@@ -934,6 +937,7 @@ public:
         consensus.nStakeMinAge = 0;
         consensus.nBlockStakeModifierV1A = consensus.nPosStartHeight;
         consensus.nBlockStakeModifierV2 = consensus.V16DeploymentHeight;
+        consensus.strCarbonOffsetAddress = "TqMgq4qkw7bGxf6CDhtDfEqzEtWD5C7x8U";
         // ATP parameters
         consensus.ATPStartHeight = consensus.V16DeploymentHeight;
         consensus.WgrAddrPrefix = "wgrreg";
