bytzck/BYTZ

Update Zerocoin

src/pos/stakeinput.h
@@ -38,6 +38,43 @@ public:
     }
 };
 
+
+// zStake can take two forms
+// 1) the stake candidate, which is a zcmint that is attempted to be staked
+// 2) a staked zbytz, which is a zcspend that has successfully staked
+class CZStake : public CStakeInput
+{
+private:
+    uint32_t nChecksum;
+    bool fMint;
+    libzerocoin::CoinDenomination denom;
+    uint256 hashSerial;
+
+public:
+    explicit CZStake(libzerocoin::CoinDenomination denom, const uint256& hashSerial)
+    {
+        this->denom = denom;
+        this->hashSerial = hashSerial;
+        fMint = true;
+    }
+
+    explicit CZStake(const libzerocoin::CoinSpend& spend);
+
+    CBlockIndex* GetIndexFrom() override;
+    bool GetTxFrom(CTransactionRef& tx) override;
+    CAmount GetValue() override;
+    bool GetModifier(uint64_t& nStakeModifier) override;
+    CDataStream GetUniqueness() override;
+    bool CreateTxIn(CWallet* pwallet, CTxIn& txIn, uint256 hashTxOut = uint256()) override;
+    bool CreateTxOuts(CWallet* pwallet, std::vector<CTxOut>& vout, CAmount nTotal) override;
+//    bool MarkSpent(CWallet* pwallet, const uint256& txid);
+    bool IsZBYTZ() override { return true; }
+    uint256 GetSerialHash() const override { return hashSerial; }
+    int GetChecksumHeightFromMint();
+    int GetChecksumHeightFromSpend();
+    uint32_t GetChecksum();
+};
+
 class CStake : public CStakeInput
 {
 private:

src/txdb.cpp
@@ -13,6 +13,7 @@
 #include <util.h>
 #include <ui_interface.h>
 #include <init.h>
+#include <zbytz/accumulators.h>
 
 #include <stdint.h>
 
@@ -420,6 +421,7 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
     pcursor->Seek(std::make_pair(DB_BLOCK_INDEX, uint256()));
 
     // Load mapBlockIndex
+    uint256 nPreviousCheckpoint;
     while (pcursor->Valid()) {
         boost::this_thread::interruption_point();
         std::pair<char, uint256> key;
@@ -443,10 +445,27 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
 
                 //Proof Of Stake
                 pindexNew->nFlags         = diskindex.nFlags;
+                if (pindexNew->nHeight < Params().GetConsensus().nBlockStakeModifierV2) {
+                    pindexNew->nStakeModifier = diskindex.nStakeModifier;
+                } else {
+                    pindexNew->nStakeModifierV2 = diskindex.nStakeModifierV2;
+                }
+                //Zerocoin
+                pindexNew->nAccumulatorCheckpoint = diskindex.nAccumulatorCheckpoint;
+                pindexNew->mapZerocoinSupply = diskindex.mapZerocoinSupply;
+                pindexNew->vMintDenominationsInBlock = diskindex.vMintDenominationsInBlock;
 
                 if (pindexNew->nHeight < consensusParams.nPosStartHeight && pindexNew->IsProofOfWork() && !CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, consensusParams))
                     return error("%s: CheckProofOfWork failed: %s", __func__, pindexNew->ToString());
 
+                //populate accumulator checksum map in memory
+                if(pindexNew->nAccumulatorCheckpoint != uint256() && pindexNew->nAccumulatorCheckpoint != nPreviousCheckpoint) {
+                    //Don't load any checkpoints that exist before v2 zbytz. The accumulator is invalid for v1 and not used.
+                    if (pindexNew->nHeight >= Params().GetConsensus().nBlockZerocoinV2)
+                        LoadAccumulatorValuesFromDB(pindexNew->nAccumulatorCheckpoint);
+
+                    nPreviousCheckpoint = pindexNew->nAccumulatorCheckpoint;
+                }
                 pcursor->Next();
             } else {
                 return error("%s: failed to read value", __func__);

src/zbytz/accumulatorcheckpoints.cpp
@@ -1,0 +1,84 @@
+// Copyright (c) 2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "zbytz/accumulatorcheckpoints.h"
+#include "zbytz/accumulatorcheckpoints.json.h"
+
+namespace AccumulatorCheckpoints
+{
+    std::map<int, Checkpoint> mapCheckpoints;
+
+    UniValue read_json(const std::string& jsondata)
+    {
+        UniValue v;
+
+        if (!v.read(jsondata) || !v.isArray())
+        {
+            return UniValue(UniValue::VARR);
+        }
+        return v.get_array();
+    }
+
+    bool LoadCheckpoints(const std::string& strNetwork)
+    {
+        UniValue v;
+        if (strNetwork == "main")
+            v = read_json(GetMainCheckpoints());
+        else if (strNetwork == "test")
+            v = read_json(GetTestCheckpoints());
+        else if (strNetwork == "regtest")
+            v = read_json(GetRegTestCheckpoints());
+        else
+            return false;
+
+        if (v.empty())
+            return false;
+
+        for (unsigned int idx = 0; idx < v.size(); idx++) {
+            const UniValue &val = v[idx];
+            const UniValue &o = val.get_obj();
+
+            const UniValue &vHeight = find_value(o, "height");
+            if (!vHeight.isNum())
+                return false;
+
+            int nHeight = vHeight.get_int();
+            if (nHeight < 0)
+                return false;
+
+            Checkpoint checkpoint;
+            for (auto denom : libzerocoin::zerocoinDenomList) {
+                const UniValue& vDenomValue = find_value(o, std::to_string(denom));
+                if (!vDenomValue.isStr()) {
+                    return false;
+                }
+                CBigNum bn = 0;
+                bn.SetHex(vDenomValue.get_str());
+                checkpoint.insert(std::make_pair(denom, bn));
+            }
+
+            mapCheckpoints.insert(std::make_pair(nHeight, checkpoint));
+        }
+        return true;
+    }
+
+    Checkpoint GetClosestCheckpoint(const int& nHeight, int& nHeightCheckpoint)
+    {
+        nHeightCheckpoint = -1;
+        for (auto it : mapCheckpoints) {
+            //only checkpoints that are less than the height requested (unless height is less than the first checkpoint)
+            if (it.first < nHeight) {
+                if (nHeightCheckpoint == -1)
+                    nHeightCheckpoint = it.first;
+                if (nHeight - it.first < nHeightCheckpoint)
+                    nHeightCheckpoint = it.first;
+            }
+        }
+
+        if (nHeightCheckpoint != -1)
+            return mapCheckpoints.at(nHeightCheckpoint);
+
+        return Checkpoint();
+    }
+}

src/zbytz/accumulatorcheckpoints.json.h
@@ -1,0 +1,78 @@
+// Copyright (c) 2018 The PIVX developers
+// Copyright (c) 2018 The ION developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ZBYTZ_ACCUMULATORCHECKPOINTS_JSON_H
+#define ZBYTZ_ACCUMULATORCHECKPOINTS_JSON_H
+
+#include <string>
+std::string GetMainCheckpoints() {
+    std::string strMainCheckpoints = "[\n"
+            "  {\n"
+            "    \"height\": 0,\n"
+            "    \"1\": \"0\",\n"
+            "    \"5\": \"0\",\n"
+            "    \"10\": \"0\",\n"
+            "    \"50\": \"0\",\n"
+            "    \"100\": \"0\",\n"
+            "    \"500\": \"0\",\n"
+            "    \"1000\": \"0\",\n"
+            "    \"5000\": \"0\"\n"
+            "  }\n"
+            "]";
+    return strMainCheckpoints;
+}
+
+std::string GetTestCheckpoints() {
+    std::string strTestCheckpoints = "[\n"
+            "  {\n"
+            "    \"height\": 300,\n"
+            "    \"1\": \"3c1\",\n"
+            "    \"5\": \"3c1\",\n"
+            "    \"10\": \"3c1\",\n"
+            "    \"50\": \"3c1\",\n"
+            "    \"100\": \"3c1\",\n"
+            "    \"500\": \"3c1\",\n"
+            "    \"1000\": \"3c1\",\n"
+            "    \"5000\": \"3c1\"\n"
+            "  }\n"
+            "]";
+    return strTestCheckpoints;
+}
+
+std::string GetRegTestCheckpoints() {
+    std::string strRegTestCheckpoints = "[\n"
+            "  {\n"
+            "    \"height\": 0,\n"
+            "    \"1\": \"0\",\n"
+            "    \"5\": \"0\",\n"
+            "    \"10\": \"0\",\n"
+            "    \"50\": \"0\",\n"
+            "    \"100\": \"0\",\n"
+            "    \"500\": \"0\",\n"
+            "    \"1000\": \"0\",\n"
+            "    \"5000\": \"0\"\n"
+            "  }\n"
+            "]";
+    return strRegTestCheckpoints;
+}
+
+std::string GetDevNetCheckpoints() {
+    std::string strDevNetCheckpoints = "[\n"
+            "  {\n"
+            "    \"height\": 0,\n"
+            "    \"1\": \"0\",\n"
+            "    \"5\": \"0\",\n"
+            "    \"10\": \"0\",\n"
+            "    \"50\": \"0\",\n"
+            "    \"100\": \"0\",\n"
+            "    \"500\": \"0\",\n"
+            "    \"1000\": \"0\",\n"
+            "    \"5000\": \"0\"\n"
+            "  }\n"
+            "]";
+    return strDevNetCheckpoints;
+}
+
+#endif //ZBYTZ_ACCUMULATORCHECKPOINTS_JSON_H

src/zbytz/accumulatormap.cpp
@@ -1,0 +1,101 @@
+// Copyright (c) 2017-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "libzerocoin/Denominations.h"
+#include "txdb.h"
+#include "validation.h"
+#include "zbytz/accumulatormap.h"
+#include "zbytz/accumulators.h"
+#include "zbytz/zerocoindb.h"
+
+//Construct accumulators for all denominations
+AccumulatorMap::AccumulatorMap(libzerocoin::ZerocoinParams* params)
+{
+    this->params = params;
+    for (auto& denom : libzerocoin::zerocoinDenomList) {
+        std::unique_ptr<libzerocoin::Accumulator> uptr(new libzerocoin::Accumulator(params, denom));
+        mapAccumulators.insert(std::make_pair(denom, std::move(uptr)));
+    }
+}
+
+//Reset each accumulator to its default state
+void AccumulatorMap::Reset()
+{
+    Reset(params);
+}
+
+void AccumulatorMap::Reset(libzerocoin::ZerocoinParams* params2)
+{
+    this->params = params2;
+    mapAccumulators.clear();
+    for (auto& denom : libzerocoin::zerocoinDenomList) {
+        std::unique_ptr<libzerocoin::Accumulator> uptr(new libzerocoin::Accumulator(params2, denom));
+        mapAccumulators.insert(std::make_pair(denom, std::move(uptr)));
+    }
+}
+
+//Load a checkpoint containing 8 32bit checksums of accumulator values.
+bool AccumulatorMap::Load(uint256 nCheckpoint)
+{
+    for (auto& denom : libzerocoin::zerocoinDenomList) {
+        uint32_t nChecksum = ParseChecksum(nCheckpoint, denom);
+
+        CBigNum bnValue;
+        if (!zerocoinDB->ReadAccumulatorValue(nChecksum, bnValue))
+            return error("%s : cannot find checksum %d", __func__, nChecksum);
+
+        mapAccumulators.at(denom)->setValue(bnValue);
+    }
+    return true;
+}
+
+//Load accumulator map from a hard-checkpoint
+void AccumulatorMap::Load(const AccumulatorCheckpoints::Checkpoint& checkpoint)
+{
+     for (auto it : checkpoint)
+         mapAccumulators.at(it.first)->setValue(it.second);
+}
+
+//Add a zerocoin to the accumulator of its denomination.
+bool AccumulatorMap::Accumulate(const libzerocoin::PublicCoin& pubCoin, bool fSkipValidation)
+{
+    libzerocoin::CoinDenomination denom = pubCoin.getDenomination();
+    if (denom == libzerocoin::CoinDenomination::ZQ_ERROR)
+        return false;
+
+    if (fSkipValidation)
+        mapAccumulators.at(denom)->increment(pubCoin.getValue());
+    else
+        mapAccumulators.at(denom)->accumulate(pubCoin);
+    return true;
+}
+
+libzerocoin::Accumulator AccumulatorMap::GetAccumulator(libzerocoin::CoinDenomination denom)
+{
+    return libzerocoin::Accumulator(params, denom, GetValue(denom));
+}
+
+//Get the value of a specific accumulator
+CBigNum AccumulatorMap::GetValue(libzerocoin::CoinDenomination denom)
+{
+    if (denom == libzerocoin::CoinDenomination::ZQ_ERROR)
+        return CBigNum(0);
+    return mapAccumulators.at(denom)->getValue();
+}
+
+//Calculate a 32bit checksum of each accumulator value. Concatenate checksums into uint256
+uint256 AccumulatorMap::GetCheckpoint()
+{
+    arith_uint256 nCheckpoint;
+
+    //Prevent possible overflows from future changes to the list and forgetting to update this code
+    assert(libzerocoin::zerocoinDenomList.size() == 8);
+    for (auto& denom : libzerocoin::zerocoinDenomList) {
+        CBigNum bnValue = mapAccumulators.at(denom)->getValue();
+        uint32_t nCheckSum = GetChecksum(bnValue);
+        nCheckpoint = nCheckpoint << 32 | nCheckSum;
+    }
+
+    return ArithToUint256(nCheckpoint);
+}

src/zbytz/accumulatormap.h
@@ -1,0 +1,29 @@
+// Copyright (c) 2017-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ZBYTZ_ACCUMULATORMAP_H
+#define ZBYTZ_ACCUMULATORMAP_H
+
+#include "libzerocoin/Accumulator.h"
+#include "libzerocoin/Coin.h"
+#include "zbytz/accumulatorcheckpoints.h"
+
+//A map with an accumulator for each denomination
+class AccumulatorMap
+{
+private:
+    libzerocoin::ZerocoinParams* params;
+    std::map<libzerocoin::CoinDenomination, std::unique_ptr<libzerocoin::Accumulator> > mapAccumulators;
+public:
+    explicit AccumulatorMap(libzerocoin::ZerocoinParams* params);
+    bool Load(uint256 nCheckpoint);
+    void Load(const AccumulatorCheckpoints::Checkpoint& checkpoint);
+    bool Accumulate(const libzerocoin::PublicCoin& pubCoin, bool fSkipValidation = false);
+    libzerocoin::Accumulator GetAccumulator(libzerocoin::CoinDenomination denom);
+    CBigNum GetValue(libzerocoin::CoinDenomination denom);
+    uint256 GetCheckpoint();
+    void Reset();
+    void Reset(libzerocoin::ZerocoinParams* params2);
+};
+#endif //ZBYTZ_ACCUMULATORMAP_H

ckti-dash-current/dash

src/pos/stakeinput.h
@@ -38,6 +38,43 @@ public:
     }
 };
 
+
+// zStake can take two forms
+// 1) the stake candidate, which is a zcmint that is attempted to be staked
+// 2) a staked zwagerr, which is a zcspend that has successfully staked
+class CZStake : public CStakeInput
+{
+private:
+    uint32_t nChecksum;
+    bool fMint;
+    libzerocoin::CoinDenomination denom;
+    uint256 hashSerial;
+
+public:
+    explicit CZStake(libzerocoin::CoinDenomination denom, const uint256& hashSerial)
+    {
+        this->denom = denom;
+        this->hashSerial = hashSerial;
+        fMint = true;
+    }
+
+    explicit CZStake(const libzerocoin::CoinSpend& spend);
+
+    CBlockIndex* GetIndexFrom() override;
+    bool GetTxFrom(CTransactionRef& tx) override;
+    CAmount GetValue() override;
+    bool GetModifier(uint64_t& nStakeModifier) override;
+    CDataStream GetUniqueness() override;
+    bool CreateTxIn(CWallet* pwallet, CTxIn& txIn, uint256 hashTxOut = uint256()) override;
+    bool CreateTxOuts(CWallet* pwallet, std::vector<CTxOut>& vout, CAmount nTotal) override;
+//    bool MarkSpent(CWallet* pwallet, const uint256& txid);
+    bool IsZWAGERR() override { return true; }
+    uint256 GetSerialHash() const override { return hashSerial; }
+    int GetChecksumHeightFromMint();
+    int GetChecksumHeightFromSpend();
+    uint32_t GetChecksum();
+};
+
 class CStake : public CStakeInput
 {
 private:

src/txdb.cpp
@@ -13,6 +13,7 @@
 #include <util.h>
 #include <ui_interface.h>
 #include <init.h>
+#include <zwagerr/accumulators.h>
 
 #include <stdint.h>
 
@@ -420,6 +421,7 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
     pcursor->Seek(std::make_pair(DB_BLOCK_INDEX, uint256()));
 
     // Load mapBlockIndex
+    uint256 nPreviousCheckpoint;
     while (pcursor->Valid()) {
         boost::this_thread::interruption_point();
         std::pair<char, uint256> key;
@@ -443,10 +445,27 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
 
                 //Proof Of Stake
                 pindexNew->nFlags         = diskindex.nFlags;
+                if (pindexNew->nHeight < Params().GetConsensus().nBlockStakeModifierV2) {
+                    pindexNew->nStakeModifier = diskindex.nStakeModifier;
+                } else {
+                    pindexNew->nStakeModifierV2 = diskindex.nStakeModifierV2;
+                }
+                //Zerocoin
+                pindexNew->nAccumulatorCheckpoint = diskindex.nAccumulatorCheckpoint;
+                pindexNew->mapZerocoinSupply = diskindex.mapZerocoinSupply;
+                pindexNew->vMintDenominationsInBlock = diskindex.vMintDenominationsInBlock;
 
                 if (pindexNew->nHeight < consensusParams.nPosStartHeight && pindexNew->IsProofOfWork() && !CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, consensusParams))
                     return error("%s: CheckProofOfWork failed: %s", __func__, pindexNew->ToString());
 
+                //populate accumulator checksum map in memory
+                if(pindexNew->nAccumulatorCheckpoint != uint256() && pindexNew->nAccumulatorCheckpoint != nPreviousCheckpoint) {
+                    //Don't load any checkpoints that exist before v2 zwagerr. The accumulator is invalid for v1 and not used.
+                    if (pindexNew->nHeight >= Params().GetConsensus().nBlockZerocoinV2)
+                        LoadAccumulatorValuesFromDB(pindexNew->nAccumulatorCheckpoint);
+
+                    nPreviousCheckpoint = pindexNew->nAccumulatorCheckpoint;
+                }
                 pcursor->Next();
             } else {
                 return error("%s: failed to read value", __func__);

src/zwagerr/accumulatorcheckpoints.cpp
@@ -1,0 +1,84 @@
+// Copyright (c) 2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "zwagerr/accumulatorcheckpoints.h"
+#include "zwagerr/accumulatorcheckpoints.json.h"
+
+namespace AccumulatorCheckpoints
+{
+    std::map<int, Checkpoint> mapCheckpoints;
+
+    UniValue read_json(const std::string& jsondata)
+    {
+        UniValue v;
+
+        if (!v.read(jsondata) || !v.isArray())
+        {
+            return UniValue(UniValue::VARR);
+        }
+        return v.get_array();
+    }
+
+    bool LoadCheckpoints(const std::string& strNetwork)
+    {
+        UniValue v;
+        if (strNetwork == "main")
+            v = read_json(GetMainCheckpoints());
+        else if (strNetwork == "test")
+            v = read_json(GetTestCheckpoints());
+        else if (strNetwork == "regtest")
+            v = read_json(GetRegTestCheckpoints());
+        else
+            return false;
+
+        if (v.empty())
+            return false;
+
+        for (unsigned int idx = 0; idx < v.size(); idx++) {
+            const UniValue &val = v[idx];
+            const UniValue &o = val.get_obj();
+
+            const UniValue &vHeight = find_value(o, "height");
+            if (!vHeight.isNum())
+                return false;
+
+            int nHeight = vHeight.get_int();
+            if (nHeight < 0)
+                return false;
+
+            Checkpoint checkpoint;
+            for (auto denom : libzerocoin::zerocoinDenomList) {
+                const UniValue& vDenomValue = find_value(o, std::to_string(denom));
+                if (!vDenomValue.isStr()) {
+                    return false;
+                }
+                CBigNum bn = 0;
+                bn.SetHex(vDenomValue.get_str());
+                checkpoint.insert(std::make_pair(denom, bn));
+            }
+
+            mapCheckpoints.insert(std::make_pair(nHeight, checkpoint));
+        }
+        return true;
+    }
+
+    Checkpoint GetClosestCheckpoint(const int& nHeight, int& nHeightCheckpoint)
+    {
+        nHeightCheckpoint = -1;
+        for (auto it : mapCheckpoints) {
+            //only checkpoints that are less than the height requested (unless height is less than the first checkpoint)
+            if (it.first < nHeight) {
+                if (nHeightCheckpoint == -1)
+                    nHeightCheckpoint = it.first;
+                if (nHeight - it.first < nHeightCheckpoint)
+                    nHeightCheckpoint = it.first;
+            }
+        }
+
+        if (nHeightCheckpoint != -1)
+            return mapCheckpoints.at(nHeightCheckpoint);
+
+        return Checkpoint();
+    }
+}

src/zwagerr/accumulatorcheckpoints.json.h
@@ -1,0 +1,78 @@
+// Copyright (c) 2018 The PIVX developers
+// Copyright (c) 2018 The ION developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ZWAGERR_ACCUMULATORCHECKPOINTS_JSON_H
+#define ZWAGERR_ACCUMULATORCHECKPOINTS_JSON_H
+
+#include <string>
+std::string GetMainCheckpoints() {
+    std::string strMainCheckpoints = "[\n"
+            "  {\n"
+            "    \"height\": 0,\n"
+            "    \"1\": \"0\",\n"
+            "    \"5\": \"0\",\n"
+            "    \"10\": \"0\",\n"
+            "    \"50\": \"0\",\n"
+            "    \"100\": \"0\",\n"
+            "    \"500\": \"0\",\n"
+            "    \"1000\": \"0\",\n"
+            "    \"5000\": \"0\"\n"
+            "  }\n"
+            "]";
+    return strMainCheckpoints;
+}
+
+std::string GetTestCheckpoints() {
+    std::string strTestCheckpoints = "[\n"
+            "  {\n"
+            "    \"height\": 300,\n"
+            "    \"1\": \"3c1\",\n"
+            "    \"5\": \"3c1\",\n"
+            "    \"10\": \"3c1\",\n"
+            "    \"50\": \"3c1\",\n"
+            "    \"100\": \"3c1\",\n"
+            "    \"500\": \"3c1\",\n"
+            "    \"1000\": \"3c1\",\n"
+            "    \"5000\": \"3c1\"\n"
+            "  }\n"
+            "]";
+    return strTestCheckpoints;
+}
+
+std::string GetRegTestCheckpoints() {
+    std::string strRegTestCheckpoints = "[\n"
+            "  {\n"
+            "    \"height\": 0,\n"
+            "    \"1\": \"0\",\n"
+            "    \"5\": \"0\",\n"
+            "    \"10\": \"0\",\n"
+            "    \"50\": \"0\",\n"
+            "    \"100\": \"0\",\n"
+            "    \"500\": \"0\",\n"
+            "    \"1000\": \"0\",\n"
+            "    \"5000\": \"0\"\n"
+            "  }\n"
+            "]";
+    return strRegTestCheckpoints;
+}
+
+std::string GetDevNetCheckpoints() {
+    std::string strDevNetCheckpoints = "[\n"
+            "  {\n"
+            "    \"height\": 0,\n"
+            "    \"1\": \"0\",\n"
+            "    \"5\": \"0\",\n"
+            "    \"10\": \"0\",\n"
+            "    \"50\": \"0\",\n"
+            "    \"100\": \"0\",\n"
+            "    \"500\": \"0\",\n"
+            "    \"1000\": \"0\",\n"
+            "    \"5000\": \"0\"\n"
+            "  }\n"
+            "]";
+    return strDevNetCheckpoints;
+}
+
+#endif //ZWAGERR_ACCUMULATORCHECKPOINTS_JSON_H

src/zwagerr/accumulatormap.cpp
@@ -1,0 +1,101 @@
+// Copyright (c) 2017-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "libzerocoin/Denominations.h"
+#include "txdb.h"
+#include "validation.h"
+#include "zwagerr/accumulatormap.h"
+#include "zwagerr/accumulators.h"
+#include "zwagerr/zerocoindb.h"
+
+//Construct accumulators for all denominations
+AccumulatorMap::AccumulatorMap(libzerocoin::ZerocoinParams* params)
+{
+    this->params = params;
+    for (auto& denom : libzerocoin::zerocoinDenomList) {
+        std::unique_ptr<libzerocoin::Accumulator> uptr(new libzerocoin::Accumulator(params, denom));
+        mapAccumulators.insert(std::make_pair(denom, std::move(uptr)));
+    }
+}
+
+//Reset each accumulator to its default state
+void AccumulatorMap::Reset()
+{
+    Reset(params);
+}
+
+void AccumulatorMap::Reset(libzerocoin::ZerocoinParams* params2)
+{
+    this->params = params2;
+    mapAccumulators.clear();
+    for (auto& denom : libzerocoin::zerocoinDenomList) {
+        std::unique_ptr<libzerocoin::Accumulator> uptr(new libzerocoin::Accumulator(params2, denom));
+        mapAccumulators.insert(std::make_pair(denom, std::move(uptr)));
+    }
+}
+
+//Load a checkpoint containing 8 32bit checksums of accumulator values.
+bool AccumulatorMap::Load(uint256 nCheckpoint)
+{
+    for (auto& denom : libzerocoin::zerocoinDenomList) {
+        uint32_t nChecksum = ParseChecksum(nCheckpoint, denom);
+
+        CBigNum bnValue;
+        if (!zerocoinDB->ReadAccumulatorValue(nChecksum, bnValue))
+            return error("%s : cannot find checksum %d", __func__, nChecksum);
+
+        mapAccumulators.at(denom)->setValue(bnValue);
+    }
+    return true;
+}
+
+//Load accumulator map from a hard-checkpoint
+void AccumulatorMap::Load(const AccumulatorCheckpoints::Checkpoint& checkpoint)
+{
+     for (auto it : checkpoint)
+         mapAccumulators.at(it.first)->setValue(it.second);
+}
+
+//Add a zerocoin to the accumulator of its denomination.
+bool AccumulatorMap::Accumulate(const libzerocoin::PublicCoin& pubCoin, bool fSkipValidation)
+{
+    libzerocoin::CoinDenomination denom = pubCoin.getDenomination();
+    if (denom == libzerocoin::CoinDenomination::ZQ_ERROR)
+        return false;
+
+    if (fSkipValidation)
+        mapAccumulators.at(denom)->increment(pubCoin.getValue());
+    else
+        mapAccumulators.at(denom)->accumulate(pubCoin);
+    return true;
+}
+
+libzerocoin::Accumulator AccumulatorMap::GetAccumulator(libzerocoin::CoinDenomination denom)
+{
+    return libzerocoin::Accumulator(params, denom, GetValue(denom));
+}
+
+//Get the value of a specific accumulator
+CBigNum AccumulatorMap::GetValue(libzerocoin::CoinDenomination denom)
+{
+    if (denom == libzerocoin::CoinDenomination::ZQ_ERROR)
+        return CBigNum(0);
+    return mapAccumulators.at(denom)->getValue();
+}
+
+//Calculate a 32bit checksum of each accumulator value. Concatenate checksums into uint256
+uint256 AccumulatorMap::GetCheckpoint()
+{
+    arith_uint256 nCheckpoint;
+
+    //Prevent possible overflows from future changes to the list and forgetting to update this code
+    assert(libzerocoin::zerocoinDenomList.size() == 8);
+    for (auto& denom : libzerocoin::zerocoinDenomList) {
+        CBigNum bnValue = mapAccumulators.at(denom)->getValue();
+        uint32_t nCheckSum = GetChecksum(bnValue);
+        nCheckpoint = nCheckpoint << 32 | nCheckSum;
+    }
+
+    return ArithToUint256(nCheckpoint);
+}

src/zwagerr/accumulatormap.h
@@ -1,0 +1,29 @@
+// Copyright (c) 2017-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ZWAGERR_ACCUMULATORMAP_H
+#define ZWAGERR_ACCUMULATORMAP_H
+
+#include "libzerocoin/Accumulator.h"
+#include "libzerocoin/Coin.h"
+#include "zwagerr/accumulatorcheckpoints.h"
+
+//A map with an accumulator for each denomination
+class AccumulatorMap
+{
+private:
+    libzerocoin::ZerocoinParams* params;
+    std::map<libzerocoin::CoinDenomination, std::unique_ptr<libzerocoin::Accumulator> > mapAccumulators;
+public:
+    explicit AccumulatorMap(libzerocoin::ZerocoinParams* params);
+    bool Load(uint256 nCheckpoint);
+    void Load(const AccumulatorCheckpoints::Checkpoint& checkpoint);
+    bool Accumulate(const libzerocoin::PublicCoin& pubCoin, bool fSkipValidation = false);
+    libzerocoin::Accumulator GetAccumulator(libzerocoin::CoinDenomination denom);
+    CBigNum GetValue(libzerocoin::CoinDenomination denom);
+    uint256 GetCheckpoint();
+    void Reset();
+    void Reset(libzerocoin::ZerocoinParams* params2);
+};
+#endif //ZWAGERR_ACCUMULATORMAP_H