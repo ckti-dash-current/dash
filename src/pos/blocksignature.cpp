ioncoincore/ion

Load POS blocks
- Add POS flags to CBlockIndex
- Set coinbase maturity to 10 for blocks with height < 10
- Skip time checks before Midas activation
- Add validation and signature functions
- Adapt block format

src/pos/blocksignature.cpp
@@ -1,0 +1,95 @@
+// Copyright (c) 2017-2018 The PIVX developers
+// Copyright (c) 2018-2019 The Ion developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "blocksignature.h"
+#include "util.h"
+#include "utilstrencodings.h"
+//#include "main.h"
+//#include "xionchain.h"
+
+typedef std::vector<unsigned char> valtype;
+
+bool SignBlockWithKey(CBlock& block, const CKey& key)
+{
+    if (!key.Sign(block.GetHash(), block.vchBlockSig))
+        return error("%s: failed to sign block hash with key", __func__);
+    return true;
+}
+
+bool GetKeyIDFromUTXO(const CTxOut& txout, CKeyID& keyID)
+{
+    std::vector<valtype> vSolutions;
+    txnouttype whichType;
+    if (!Solver(txout.scriptPubKey, whichType, vSolutions))
+        return false;
+    if (whichType == TX_PUBKEY) {
+        keyID = CPubKey(vSolutions[0]).GetID();
+    } else if (whichType == TX_PUBKEYHASH/* || whichType == TX_GRP_PUBKEYHASH*/) {
+        keyID = CKeyID(uint160(vSolutions[0]));
+    }
+    return true;
+}
+
+bool SignBlock(CBlock& block, const CKeyStore& keystore)
+{
+    CKeyID keyID;
+    if (block.IsProofOfWork()) {
+        bool fFoundID = false;
+        for (const CTxOut& txout :block.vtx[0]->vout) {
+            if (!GetKeyIDFromUTXO(txout, keyID))
+                continue;
+            fFoundID = true;
+            break;
+        }
+        if (!fFoundID)
+            return error("%s: failed to find key for PoW", __func__);
+    } else {
+        if (!GetKeyIDFromUTXO(block.vtx[1]->vout[1], keyID))
+            return error("%s: failed to find key for PoS", __func__);
+    }
+
+    CKey key;
+    if (!keystore.GetKey(keyID, key))
+        return error("%s: failed to get key from keystore", __func__);
+
+    return SignBlockWithKey(block, key);
+}
+
+bool CheckBlockSignature(const CBlock& block)
+{
+    if (block.IsProofOfWork())
+        return block.vchBlockSig.empty();
+
+    if (block.vchBlockSig.empty())
+        return error("%s: vchBlockSig is empty!", __func__);
+
+    /** Each block is signed by the private key of the input that is staked. This can be either xION or normal UTXO
+     *  xION: Each xION has a keypair associated with it. The serial number is a hash of the public key.
+     *  UTXO: The public key that signs must match the public key associated with the first utxo of the coinstake tx.
+     */
+    CPubKey pubkey;
+    bool fxIONStake = block.vtx[1]->vin[0].IsZerocoinSpend();
+    if (fxIONStake) {
+/*
+        libzerocoin::CoinSpend spend = TxInToZerocoinSpend(block.vtx[1].vin[0]);
+        pubkey = spend.getPubKey();
+*/
+        return error ("%s: zerocoin functionality not implemented yet", __func__);
+    } else {
+        txnouttype whichType;
+        std::vector<valtype> vSolutions;
+        const CTxOut& txout = block.vtx[1]->vout[1];
+        if (!Solver(txout.scriptPubKey, whichType, vSolutions))
+            return false;
+        if (whichType == TX_PUBKEY || whichType == TX_PUBKEYHASH) {
+            valtype& vchPubKey = vSolutions[0];
+            pubkey = CPubKey(vchPubKey);
+        }
+    }
+    if (!pubkey.IsValid())
+        return error("%s: invalid pubkey %s", __func__, HexStr(pubkey));
+
+    return pubkey.Verify(block.GetHash(), block.vchBlockSig);
+}

src/txdb.cpp
@@ -423,7 +423,10 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
                 pindexNew->nStatus        = diskindex.nStatus;
                 pindexNew->nTx            = diskindex.nTx;
 
-                if (!CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, consensusParams))
+                //Proof Of Stake
+                pindexNew->nFlags         = diskindex.nFlags;
+
+                if (pindexNew->nHeight < consensusParams.POSStartHeight && pindexNew->IsProofOfWork() && !CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, consensusParams))
                     return error("%s: CheckProofOfWork failed: %s", __func__, pindexNew->ToString());
 
                 pcursor->Next();

ckti-dash-current/dash

src/pos/blocksignature.cpp
@@ -1,0 +1,95 @@
+// Copyright (c) 2017-2018 The PIVX developers
+// Copyright (c) 2018-2019 The Ion developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "blocksignature.h"
+#include "util.h"
+#include "utilstrencodings.h"
+//#include "main.h"
+//#include "zwagerrchain.h"
+
+typedef std::vector<unsigned char> valtype;
+
+bool SignBlockWithKey(CBlock& block, const CKey& key)
+{
+    if (!key.Sign(block.GetHash(), block.vchBlockSig))
+        return error("%s: failed to sign block hash with key", __func__);
+    return true;
+}
+
+bool GetKeyIDFromUTXO(const CTxOut& txout, CKeyID& keyID)
+{
+    std::vector<valtype> vSolutions;
+    txnouttype whichType;
+    if (!Solver(txout.scriptPubKey, whichType, vSolutions))
+        return false;
+    if (whichType == TX_PUBKEY) {
+        keyID = CPubKey(vSolutions[0]).GetID();
+    } else if (whichType == TX_PUBKEYHASH/* || whichType == TX_GRP_PUBKEYHASH*/) {
+        keyID = CKeyID(uint160(vSolutions[0]));
+    }
+    return true;
+}
+
+bool SignBlock(CBlock& block, const CKeyStore& keystore)
+{
+    CKeyID keyID;
+    if (block.IsProofOfWork()) {
+        bool fFoundID = false;
+        for (const CTxOut& txout :block.vtx[0]->vout) {
+            if (!GetKeyIDFromUTXO(txout, keyID))
+                continue;
+            fFoundID = true;
+            break;
+        }
+        if (!fFoundID)
+            return error("%s: failed to find key for PoW", __func__);
+    } else {
+        if (!GetKeyIDFromUTXO(block.vtx[1]->vout[1], keyID))
+            return error("%s: failed to find key for PoS", __func__);
+    }
+
+    CKey key;
+    if (!keystore.GetKey(keyID, key))
+        return error("%s: failed to get key from keystore", __func__);
+
+    return SignBlockWithKey(block, key);
+}
+
+bool CheckBlockSignature(const CBlock& block)
+{
+    if (block.IsProofOfWork())
+        return block.vchBlockSig.empty();
+
+    if (block.vchBlockSig.empty())
+        return error("%s: vchBlockSig is empty!", __func__);
+
+    /** Each block is signed by the private key of the input that is staked. This can be either zWAGERR or normal UTXO
+     *  zWAGERR: Each zWAGERR has a keypair associated with it. The serial number is a hash of the public key.
+     *  UTXO: The public key that signs must match the public key associated with the first utxo of the coinstake tx.
+     */
+    CPubKey pubkey;
+    bool fZStake = block.vtx[1]->vin[0].IsZerocoinSpend();
+    if (fZStake) {
+/*
+        libzerocoin::CoinSpend spend = TxInToZerocoinSpend(block.vtx[1].vin[0]);
+        pubkey = spend.getPubKey();
+*/
+        return error ("%s: zerocoin functionality not implemented yet", __func__);
+    } else {
+        txnouttype whichType;
+        std::vector<valtype> vSolutions;
+        const CTxOut& txout = block.vtx[1]->vout[1];
+        if (!Solver(txout.scriptPubKey, whichType, vSolutions))
+            return false;
+        if (whichType == TX_PUBKEY || whichType == TX_PUBKEYHASH) {
+            valtype& vchPubKey = vSolutions[0];
+            pubkey = CPubKey(vchPubKey);
+        }
+    }
+    if (!pubkey.IsValid())
+        return error("%s: invalid pubkey %s", __func__, HexStr(pubkey));
+
+    return pubkey.Verify(block.GetHash(), block.vchBlockSig);
+}

src/txdb.cpp
@@ -441,7 +441,10 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
                 pindexNew->nStatus        = diskindex.nStatus;
                 pindexNew->nTx            = diskindex.nTx;
 
-                if (!CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, consensusParams))
+                //Proof Of Stake
+                pindexNew->nFlags         = diskindex.nFlags;
+
+                if (pindexNew->nHeight < consensusParams.nPosStartHeight && pindexNew->IsProofOfWork() && !CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, consensusParams))
                     return error("%s: CheckProofOfWork failed: %s", __func__, pindexNew->ToString());
 
                 pcursor->Next();