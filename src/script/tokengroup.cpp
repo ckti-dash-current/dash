bytzck/BYTZ

Separate wallet from non-wallet in token functionality
    This restores the configure option --disable-wallet

    - Split rpctokens and rpctokenwallet
    - Split token script functionality
    - Move XDM fee management functions to wallet section
    - Reduce unnecessary include files

src/script/tokengroup.cpp
@@ -1,0 +1,93 @@
+// Copyright (c) 2015-2017 The Bitcoin Unlimited developers
+// Copyright (c) 2019 The ION Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <script/tokengroup.h>
+
+#include <consensus/tokengroups.h>
+#include <bytzaddrenc.h>
+
+class CTokenGroupID;
+
+// Token group helper functions
+
+class CTxDestinationTokenGroupExtractor : public boost::static_visitor<CTokenGroupID>
+{
+public:
+    CTokenGroupID operator()(const CKeyID &id) const { return CTokenGroupID(id); }
+    CTokenGroupID operator()(const CScriptID &id) const { return CTokenGroupID(id); }
+    CTokenGroupID operator()(const CNoDestination &) const { return CTokenGroupID(); }
+};
+
+CTokenGroupID GetTokenGroup(const CTxDestination &id)
+{
+    return boost::apply_visitor(CTxDestinationTokenGroupExtractor(), id);
+}
+
+CTokenGroupID GetTokenGroup(const std::string &addr, const CChainParams &params)
+{
+    BytzAddrContent cac = DecodeBytzAddrContent(addr, params);
+    if (cac.type == BytzAddrType::GROUP_TYPE)
+        return CTokenGroupID(cac.hash);
+    // otherwise it becomes NoGroup (i.e. data is size 0)
+    return CTokenGroupID();
+}
+
+
+class CGroupScriptVisitor : public boost::static_visitor<bool>
+{
+private:
+    CScript *script;
+    CTokenGroupID group;
+    CAmount quantity;
+
+public:
+    CGroupScriptVisitor(CTokenGroupID grp, CAmount qty, CScript *scriptin) : group(grp), quantity(qty)
+    {
+        script = scriptin;
+    }
+    bool operator()(const CNoDestination &dest) const
+    {
+        script->clear();
+        return false;
+    }
+
+    bool operator()(const CKeyID &keyID) const
+    {
+        script->clear();
+        if (group.isUserGroup())
+        {
+            *script << group.bytes() << SerializeAmount(quantity) << OP_GROUP << OP_DROP << OP_DROP << OP_DUP
+                    << OP_HASH160 << ToByteVector(keyID) << OP_EQUALVERIFY << OP_CHECKSIG;
+        }
+        else
+        {
+            *script << OP_DUP << OP_HASH160 << ToByteVector(keyID) << OP_EQUALVERIFY << OP_CHECKSIG;
+        }
+        return true;
+    }
+
+    bool operator()(const CScriptID &scriptID) const
+    {
+        script->clear();
+        if (group.isUserGroup())
+        {
+            *script << group.bytes() << SerializeAmount(quantity) << OP_GROUP << OP_DROP << OP_DROP << OP_HASH160
+                    << ToByteVector(scriptID) << OP_EQUAL;
+        }
+        else
+        {
+            *script << OP_HASH160 << ToByteVector(scriptID) << OP_EQUAL;
+        }
+        return true;
+    }
+};
+
+CScript GetScriptForDestination(const CTxDestination &dest, const CTokenGroupID &group, const CAmount &amount)
+{
+    CScript script;
+
+    boost::apply_visitor(CGroupScriptVisitor(group, amount, &script), dest);
+    return script;
+}

ckti-dash-current/dash

src/script/tokengroup.cpp
@@ -1,0 +1,93 @@
+// Copyright (c) 2015-2017 The Bitcoin Unlimited developers
+// Copyright (c) 2019 The ION Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <script/tokengroup.h>
+
+#include <consensus/tokengroups.h>
+#include <wagerraddrenc.h>
+
+class CTokenGroupID;
+
+// Token group helper functions
+
+class CTxDestinationTokenGroupExtractor : public boost::static_visitor<CTokenGroupID>
+{
+public:
+    CTokenGroupID operator()(const CKeyID &id) const { return CTokenGroupID(id); }
+    CTokenGroupID operator()(const CScriptID &id) const { return CTokenGroupID(id); }
+    CTokenGroupID operator()(const CNoDestination &) const { return CTokenGroupID(); }
+};
+
+CTokenGroupID GetTokenGroup(const CTxDestination &id)
+{
+    return boost::apply_visitor(CTxDestinationTokenGroupExtractor(), id);
+}
+
+CTokenGroupID GetTokenGroup(const std::string &addr, const CChainParams &params)
+{
+    WagerrAddrContent cac = DecodeWagerrAddrContent(addr, params);
+    if (cac.type == WagerrAddrType::GROUP_TYPE)
+        return CTokenGroupID(cac.hash);
+    // otherwise it becomes NoGroup (i.e. data is size 0)
+    return CTokenGroupID();
+}
+
+
+class CGroupScriptVisitor : public boost::static_visitor<bool>
+{
+private:
+    CScript *script;
+    CTokenGroupID group;
+    CAmount quantity;
+
+public:
+    CGroupScriptVisitor(CTokenGroupID grp, CAmount qty, CScript *scriptin) : group(grp), quantity(qty)
+    {
+        script = scriptin;
+    }
+    bool operator()(const CNoDestination &dest) const
+    {
+        script->clear();
+        return false;
+    }
+
+    bool operator()(const CKeyID &keyID) const
+    {
+        script->clear();
+        if (group.isUserGroup())
+        {
+            *script << group.bytes() << SerializeAmount(quantity) << OP_GROUP << OP_DROP << OP_DROP << OP_DUP
+                    << OP_HASH160 << ToByteVector(keyID) << OP_EQUALVERIFY << OP_CHECKSIG;
+        }
+        else
+        {
+            *script << OP_DUP << OP_HASH160 << ToByteVector(keyID) << OP_EQUALVERIFY << OP_CHECKSIG;
+        }
+        return true;
+    }
+
+    bool operator()(const CScriptID &scriptID) const
+    {
+        script->clear();
+        if (group.isUserGroup())
+        {
+            *script << group.bytes() << SerializeAmount(quantity) << OP_GROUP << OP_DROP << OP_DROP << OP_HASH160
+                    << ToByteVector(scriptID) << OP_EQUAL;
+        }
+        else
+        {
+            *script << OP_HASH160 << ToByteVector(scriptID) << OP_EQUAL;
+        }
+        return true;
+    }
+};
+
+CScript GetScriptForDestination(const CTxDestination &dest, const CTokenGroupID &group, const CAmount &amount)
+{
+    CScript script;
+
+    boost::apply_visitor(CGroupScriptVisitor(group, amount, &script), dest);
+    return script;
+}