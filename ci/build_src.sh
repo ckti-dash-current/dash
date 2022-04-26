bytzck/BYTZ

Rebrand - additional renaming

ci/build_src.sh
@@ -47,7 +47,7 @@ cd build-ci
 ../configure --cache-file=config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)
 make distdir VERSION=$BUILD_TARGET
 
-cd bytzcore-$BUILD_TARGET
+cd bytz-$BUILD_TARGET
 ./configure --cache-file=../config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)
 
 make $MAKEJOBS $GOAL || ( echo "Build failure. Verbose build follows." && make $GOAL V=1 ; false )

ci/test_unittests.sh
@@ -20,7 +20,7 @@ export LD_LIBRARY_PATH=$BUILD_DIR/depends/$HOST/lib
 export WINEDEBUG=fixme-all
 export BOOST_TEST_LOG_LEVEL=test_suite
 
-cd build-ci/bytzcore-$BUILD_TARGET
+cd build-ci/bytz-$BUILD_TARGET
 
 if [ "$DIRECT_WINE_EXEC_TESTS" = "true" ]; then
   # Inside Docker, binfmt isn't working so we can't trust in make invoking windows binaries correctly

contrib/macdeploy/custom_dsstore.py
@@ -44,8 +44,8 @@ alias = Alias.from_bytes(icvp['backgroundImageAlias'])
 alias.volume.name = package_name_ns
 alias.volume.posix_path = '/Volumes/' + package_name_ns
 alias.volume.disk_image_alias.target.filename = package_name_ns + '.temp.dmg'
-alias.volume.disk_image_alias.target.carbon_path = 'Macintosh HD:Users:\x00bytzcoreuser:\x00Documents:\x00bytzcore:\x00bytzcore:\x00' + package_name_ns + '.temp.dmg'
-alias.volume.disk_image_alias.target.posix_path = 'Users/bytzcoreuser/Documents/bytzcore/bytzcore/' + package_name_ns + '.temp.dmg'
+alias.volume.disk_image_alias.target.carbon_path = 'Macintosh HD:Users:\x00bytzuser:\x00Documents:\x00bytz:\x00bytz:\x00' + package_name_ns + '.temp.dmg'
+alias.volume.disk_image_alias.target.posix_path = 'Users/bytzuser/Documents/bytz/bytz/' + package_name_ns + '.temp.dmg'
 alias.target.carbon_path = package_name_ns + ':.background:\x00background.tiff'
 icvp['backgroundImageAlias'] = biplist.Data(alias.to_bytes())
 ds['.']['icvp'] = icvp

src/qt/utilitydialog.cpp
@@ -153,7 +153,7 @@ For more information, see the <a style=\"%2\" href=\"%3\">%1 documentation</a>."
         )
         .arg("CoinJoin")
         .arg(GUIUtil::getThemedStyleQString(GUIUtil::ThemedStyle::TS_COMMAND))
-        .arg("https://docs.bytz.gg/en/stable/wallets/bytzcore/coinjoin-instantsend.html")
+        .arg("https://docs.bytz.gg/en/stable/wallets/bytz/coinjoin-instantsend.html")
         );
         ui->aboutMessage->setWordWrap(true);
         ui->helpMessage->setVisible(false);

src/util.cpp
@@ -662,13 +662,13 @@ void PrintExceptionContinue(const std::exception_ptr pex, const char* pszExcepti
 
 fs::path GetDefaultDataDir()
 {
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\BytzCore
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\BytzCore
-    // Mac: ~/Library/Application Support/BytzCore
-    // Unix: ~/.bytzcore
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bytz
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bytz
+    // Mac: ~/Library/Application Support/Bytz
+    // Unix: ~/.bytz
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "BytzCore";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bytz";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -678,10 +678,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/BytzCore";
+    return pathRet / "Library/Application Support/Bytz";
 #else
     // Unix
-    return pathRet / ".bytzcore";
+    return pathRet / ".bytz";
 #endif
 #endif
 }

ckti-dash-current/dash

ci/build_src.sh
@@ -47,7 +47,7 @@ cd build-ci
 ../configure --cache-file=config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)
 make distdir VERSION=$BUILD_TARGET
 
-cd wagerrcore-$BUILD_TARGET
+cd wagerr-$BUILD_TARGET
 ./configure --cache-file=../config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)
 
 make $MAKEJOBS $GOAL || ( echo "Build failure. Verbose build follows." && make $GOAL V=1 ; false )

ci/test_unittests.sh
@@ -20,7 +20,7 @@ export LD_LIBRARY_PATH=$BUILD_DIR/depends/$HOST/lib
 export WINEDEBUG=fixme-all
 export BOOST_TEST_LOG_LEVEL=test_suite
 
-cd build-ci/wagerrcore-$BUILD_TARGET
+cd build-ci/wagerr-$BUILD_TARGET
 
 if [ "$DIRECT_WINE_EXEC_TESTS" = "true" ]; then
   # Inside Docker, binfmt isn't working so we can't trust in make invoking windows binaries correctly

contrib/macdeploy/custom_dsstore.py
@@ -44,8 +44,8 @@ alias = Alias.from_bytes(icvp['backgroundImageAlias'])
 alias.volume.name = package_name_ns
 alias.volume.posix_path = '/Volumes/' + package_name_ns
 alias.volume.disk_image_alias.target.filename = package_name_ns + '.temp.dmg'
-alias.volume.disk_image_alias.target.carbon_path = 'Macintosh HD:Users:\x00wagerrcoreuser:\x00Documents:\x00wagerrcore:\x00wagerrcore:\x00' + package_name_ns + '.temp.dmg'
-alias.volume.disk_image_alias.target.posix_path = 'Users/wagerrcoreuser/Documents/wagerrcore/wagerrcore/' + package_name_ns + '.temp.dmg'
+alias.volume.disk_image_alias.target.carbon_path = 'Macintosh HD:Users:\x00wagerruser:\x00Documents:\x00wagerr:\x00wagerr:\x00' + package_name_ns + '.temp.dmg'
+alias.volume.disk_image_alias.target.posix_path = 'Users/wagerruser/Documents/wagerr/wagerr/' + package_name_ns + '.temp.dmg'
 alias.target.carbon_path = package_name_ns + ':.background:\x00background.tiff'
 icvp['backgroundImageAlias'] = biplist.Data(alias.to_bytes())
 ds['.']['icvp'] = icvp

src/qt/utilitydialog.cpp
@@ -153,7 +153,7 @@ For more information, see the <a style=\"%2\" href=\"%3\">%1 documentation</a>."
         )
         .arg("CoinJoin")
         .arg(GUIUtil::getThemedStyleQString(GUIUtil::ThemedStyle::TS_COMMAND))
-        .arg("https://docs.wagerr.com/en/stable/wallets/wagerrcore/coinjoin-instantsend.html")
+        .arg("https://docs.wagerr.com/en/stable/wallets/wagerr/coinjoin-instantsend.html")
         );
         ui->aboutMessage->setWordWrap(true);
         ui->helpMessage->setVisible(false);

src/util.cpp
@@ -662,13 +662,13 @@ void PrintExceptionContinue(const std::exception_ptr pex, const char* pszExcepti
 
 fs::path GetDefaultDataDir()
 {
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\WagerrCore
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\WagerrCore
-    // Mac: ~/Library/Application Support/WagerrCore
-    // Unix: ~/.wagerrcore
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Wagerr
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Wagerr
+    // Mac: ~/Library/Application Support/Wagerr
+    // Unix: ~/.wagerr
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "WagerrCore";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Wagerr";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -678,10 +678,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/WagerrCore";
+    return pathRet / "Library/Application Support/Wagerr";
 #else
     // Unix
-    return pathRet / ".wagerrcore";
+    return pathRet / ".wagerr";
 #endif
 #endif
 }